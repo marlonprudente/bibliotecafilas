3530fe97da343336e48a06fe1f3857754cfe355a
Marlon <marlonmateuspr@gmail.com>
2017-03-24 10:59:25 -0300

    Biblioteca de Filas Sistemas Operacionais - UTFPr

--- queue.c
+++ queue.c
@@ -16,10 +16,20 @@
 
 void queue_append(queue_t **queue, queue_t *elem) {
 
-    *elem->next = **queue; //o elemento passa a apontar para o final da fila
-    *queue->next = **elem; //o antigo final da fila passa a apontar para o elemento
-    *elem->prev = **queue->prev; //o elemento passa a apontar para o antigo final da fila
-    *queue->prev = **elem; //O inicio da fila aponta para o novo final da fila
+    if (!*queue) { //Caso a fila esteja vazia, adiciona elemento a fila
+        *queue = elem;
+        elem->next = elem;
+        elem->prev = elem;
+        return;
+    }else{
+        
+    elem->next = *queue; //o elemento passa a apontar para o inicio da fila
+    elem->prev = (*queue)->prev; //o elemento passa a apontar para o antigo final da fila 
+    
+    }
+
+    
+
 };
 
 //------------------------------------------------------------------------------
@@ -32,11 +42,18 @@ void queue_append(queue_t **queue, queue_t *elem) {
 // Retorno: apontador para o elemento removido, ou NULL se erro
 
 queue_t *queue_remove(queue_t **queue, queue_t *elem) {
-    *elem->prev->next = elem->next;
-    *elem->next->prev = elem->prev;
-    *elem->next = NULL;
-    *elem->prev = NULL;
-    return elem;
+    if (!queue) {
+        return NULL;
+        
+    } else {
+        elem->prev->next = elem->next;
+        elem->next->prev = elem->prev;
+        elem->next = NULL;
+        elem->prev = NULL;
+
+        return elem;
+    }
+
 };
 
 //------------------------------------------------------------------------------
@@ -45,12 +62,23 @@ queue_t *queue_remove(queue_t **queue, queue_t *elem) {
 
 int queue_size(queue_t *queue) {
     int cont = 0;
-    queue_t aux;
-    aux = queue;
-    while (queue->next != aux->prev) {
-        cont++;
-        queue->next = queue->next->next;
+
+    if (!queue) {
+        return cont;
+    } else {
+        queue_t *aux;
+        aux = queue;
+
+        while (queue != aux->next) {
+            aux = aux->next;
+            cont++;
+            //printf("Contador: %d", &cont);
+        }
+        return cont;
     }
+
+
+
 };
 
 //------------------------------------------------------------------------------
@@ -60,4 +88,6 @@ int queue_size(queue_t *queue) {
 //
 // void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
 
-void queue_print(char *name, queue_t *queue, void print_elem(void*));
+void queue_print(char *name, queue_t *queue, void print_elem(void*)) {
+    printf("Implementando");
+};
